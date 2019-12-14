d0e98ccca787817347cae55cfe0717627aded1e1
VladimirDikovski <58719391+VladimirDikovski@users.noreply.github.com>
2019-12-14 16:28:53 +0300

    upload

---
+++ Regular Expresions/Match Full Name/Match Full Name/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            var regex = new Regex(@"\b[A-Z][a-z]+ [A-Z][a-z]+\b");
+            MatchCollection result = regex.Matches(text);
+
+            foreach (Match item in result)
+            {
+                Console.Write(item.Value+" ");
+            }
+           
+        }
+    }
+}
